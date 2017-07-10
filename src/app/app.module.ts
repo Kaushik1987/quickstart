752ff66685c5c95236eeb8b8f23db83a51e3af7a
kaushik.thanki <kaushik.thanki@clariontechnologies.co.in>
2017-07-10 19:53:33 +0530

    Inti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { FooterComponent } from "./shared/footer.component";
+import { NavComponent } from "./shared/nav.component";
+import { HomeComponent } from "./home/home.component";
+import { ErrorComponent } from "./error/error.component";
+import { AppRoutingModue } from "./shared/app.routing";
+import { AdminModule } from "./admin/admin.module";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+imports: [ 
+            BrowserModule,
+            AdminModule,
+            AppRoutingModue,            
+  ],
+  declarations: [
+                  AppComponent ,
+                  NavComponent,
+                  FooterComponent,
+                  HomeComponent,
+                  ErrorComponent                  
+                ],
+  bootstrap:    [
+                   AppComponent 
+                ]
 })
 export class AppModule { }
